40296178640d652f513edddf3883045569a25c75
Mubas Mk <mubasmk@gmail.com>
2021-02-25 19:59:30 +0530

    heroku commit

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@ const state = {
 
 module.exports.connect = (done)=>{
     //database connection
-    const url = "mongodb://localhost:27017/todoList";
+    const url = "mongodb+srv://mubasmk:ichumachu555@cluster0.wqoqx.mongodb.net/todoList";
     MongoClient.connect(url,{ useUnifiedTopology: true },(err,data)=>{
         if (err) return done(err)
         state.db = data.db("todList");
